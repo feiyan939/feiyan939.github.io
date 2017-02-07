4a1b63ef2f547f32e60346d23fd9e273d7a86482
feiyan939 <1336745158@qq.com>
2017-02-07 21:45:33 +0800

    jingjing

---
+++ tigger/static/js/college.js
@@ -0,0 +1,15 @@
+define( function( require,exports,module){
+	var collegeObj = {
+		init:function(){
+			this.htmlFn();
+		},
+		htmlFn:function(){
+            var header = require('static/view/header');
+            var footer = require('static/view/footer');
+            $('.header').html( header );
+            $('.footer').html( footer );
+		}
+
+	}
+	module.exports = collegeObj;
+})
